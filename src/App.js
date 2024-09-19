9de6d42ad4f09895aaec6db11e85167893a78f1d
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-19 16:44:57 -0300

    refactor App component and import necessary modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-  return (
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = ({message}) => {
+  return(
     <div className="App">
-      <h1>Let's create a game with React, Redux and SVG!</h1>
+      <h1>{message}</h1>
     </div>
-  );
+  )
 }
+
+App.propTypes = {
+  message: PropTypes.string.isRequired
+}
+
+
 
 export default App;
