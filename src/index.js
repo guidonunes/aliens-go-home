0bee626c89daa01162885e02dbb22b856415d2ef
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-19 17:00:09 -0300

    refactor code in the component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Game from './containers/Game/Game';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const store = configureStore(
+  reducer,
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>
+);
+
+
+registerServiceWorker();
