c12622d5ee9f998967ed719738fb46cfbaf88a89
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-21 19:26:01 -0300

    refactor sky and ground components

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -9,7 +9,7 @@ const Ground = () => {
     strokeWidth: '3px',
   };
 
-  const groundWidth = 5000;
+  const skyAndGroundWidth = 5000;
 
   return (
     <g id="ground">
@@ -17,15 +17,15 @@ const Ground = () => {
         id="ground-2"
         data-name="ground"
         style={groundStyle}
-        x={groundWidth / -2}
+        x={skyAndGroundWidth / -2}
         y={0}
-        width={groundWidth}
+        width={skyAndGroundWidth}
         height={100}
       />
       <line
-        x1={groundWidth / -2}
+        x1={skyAndGroundWidth / -2}
         y1={0}
-        x2={groundWidth / 2}
+        x2={skyAndGroundWidth / 2}
         y2={0}
         style={division}
       />
--- src/components/Sky.jsx
+++ src/components/Sky.jsx
@@ -6,15 +6,15 @@ const Sky = () => {
     fill: '#30abef',
   };
 
-  const skyWidth = 5000;
+  const skyAndGroundWidth = 5000;
   const gameHeight = 1200;
 
   return (
     <rect
       style={skyStyle}
-      x={skyWidth / -2}
+      x={skyAndGroundWidth / -2}
       y={100 - gameHeight}
-      width={skyWidth}
+      width={skyAndGroundWidth}
       height={gameHeight}
     />
   );
