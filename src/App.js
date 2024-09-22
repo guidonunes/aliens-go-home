d287951563cd2c4fcd31310f450175926d8bb77a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-22 19:57:16 -0300

    update app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Canvas from './components/Canvas';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 
-const App = ({message}) => {
+
+
+const App = ({angle, moveObjects}) => {
+  const canvasMousePosition = useRef({});
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      moveObjects(canvasMousePosition.current);
+  }, 10);
+
+  return () =>  clearInterval(intervalId);
+  }, [moveObjects]);
+
+  const trackMouse = (event) => {
+    canvasMousePosition.current = getCanvasPosition(event);
+  };
+
   return(
    <Canvas
-    angle={this.props.angle}
-    trackMouse={ event => (this.trackMouse(event))}
+    angle={angle}
+    trackMouse={ trackMouse}
    />
   )
 }
 
 App.propTypes = {
-  message: PropTypes.string.isRequired
+  angle: PropTypes.string.isRequired,
+  moveObjects: PropTypes.func.isRequired,
 }
 
 
