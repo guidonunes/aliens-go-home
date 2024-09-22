c939bd069873fcece554a8f90e88d68656b4034d
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-22 20:24:33 -0300

    fix typo

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -13,7 +13,6 @@ export const pathFromBezierCurve = (cubicBezierCurve) => {
   `;
 }
 
-
 export const radiansToDegrees = radians => ((radians * 180) / Math.PI);
 
 export const calculateAngle = (x1, y1, x2, y2) => {
@@ -38,4 +37,4 @@ export const getCanvasPosition = (event) => {
   point.y = event.clientY;
   const { x, y } = point.matrixTransform(svg.getScreenCTM().inverse());
   return { x, y };
-}
+};
