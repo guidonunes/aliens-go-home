ce72367b19e4632969fcb081eb1c6da1753df3a7
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-25 16:38:07 -0300

    imported component and add two objects

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -6,6 +6,7 @@ import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall';
 import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -29,6 +30,8 @@ const Canvas = (props) => {
       <CannonPipe rotation={props.angle} />
       <CannonBase />
       <CannonBall position={{ x: 0, y: -100 }} />
+      <FlyingObject position={{ x: -150, y: -300 }} />
+      <FlyingObject position={{ x: 150, y: -300 }} />
       <CurrentScore score={15} />
     </svg>
   );
