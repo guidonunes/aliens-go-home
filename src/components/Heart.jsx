e1f7a7a363330c1731211b426b6f99e0dba1adbb
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-25 23:11:36 -0300

    add styling to heart component

---
+++ src/components/Heart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pathFromBezierCurve } from '../utils/formulas';
+
+
+const Heart = (props) => {
+  const heartStyle = {
+    fill: '#da0d15',
+    stroke: '#a51708',
+    strokeWidth: '2px',
+  };
+}
